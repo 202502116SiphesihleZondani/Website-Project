9c87cd34b235abbcb051a688e8cdee6bad7a98cb
202502116SiphesihleZondani <202502116@spu.ac.za>
2026-04-09 15:28:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -156,7 +156,7 @@
     {
         id: 7,
         name: "Spaghetti Bolognese",
-        image: "photo-1622973536968-3ead9e780960.avif",
+        image: "https://images.unsplash.com/photo-1622973536968-3ead9e780960?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description: "Classic Italian pasta dish with a rich meat sauce.",
         category: "dinner",
         ingredients: [
